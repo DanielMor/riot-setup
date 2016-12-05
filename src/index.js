bfdd397e4c40ed34f0b127242bc4739a5e0cc885
Daniel Moreira <danielmoreira@sioslife.com>
2016-12-05 10:43:12 +0000

    Adds router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import riot from 'riot';
 import mainApp from './tags/main-app.tag';
+//import routes from './routes.js';
 
-riot.mount(mainApp);
+riot.route.stop();
+riot.route.start(true);
+
+riot.route(function(collection, id, action) {
+  console.log(collection);
+  console.log('id', id);
+  console.log('action', action);
+  riot.mount(mainApp);
+});
+
+riot.route.base('/');
